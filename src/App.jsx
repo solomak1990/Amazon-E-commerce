82247435305846f7c38d5902757fe019f3758b72
Solomon Hunegnaw <hunegnaws@gmail.com>
2025-09-17 13:14:17 +0300

    add Carousel

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
 // import { useState } from 'react'
+// import Carousel from './Components/Carousel/CarouselEffect'
 import './App.css'
 import Header from './Components/Header/Header'
+import CarouselEffect from './Components/Carousel/CarouselEffect'
 
 function App() {
     return (
     <>
               <Header />
+              <CarouselEffect />
        
       
     </>
